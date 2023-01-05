fdc890ee933ac37beb290adc01a7ff49bc6bada5
NkFauzan <121785232+NkFauzan@users.noreply.github.com>
2023-01-05 09:46:26 +0800

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,21 @@
+name =input("enter your name:")
+print(name)
+age =input("enter your age:")
+print(age)
+opinion =input("your opinion about this course in one sentence:")
+print(opinion)
+num1 = 80
+print("data type of 80 is: ",type(num1))
+num2 = 60.5
+print("data type of 60.5 is: ",type(num2))
+firstname = input("enter your first name:")
+print("my first name is: ",type(firstname))
+lastname = input("enter your last name:")
+print("my first name is: ",type(lastname))
+userage = input("enter your age")
+print("user age is: ",type(userage))
+currentage = input("enter your year birth")
+
+
+
+
